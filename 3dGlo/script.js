df46cc183ac7b93d21b78a4d68518992e80ba70d
Sennikova Alexandra <SennikovaAlexandra01@yandex.ru>
2020-07-05 13:35:27 +0300

    lesson21

--- 3dGlo/script.js
+++ 3dGlo/script.js
@@ -1,162 +1,139 @@
 window.addEventListener('DOMContentLoaded', () => {
-    
-   'use strict';
+
+    'use strict';
 
     //timer
-    
     function countTimer(deadline) {
-        let timerHours = document.querySelector('#timer-hours'),
+        const timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
-        
 
-    function getTimeRemaining(){ 
-        let dateStop = new Date(deadline).getTime(),
-        dateNow = new Date().getTime(),
-        timeRemaining = (dateStop - dateNow) / 1000,
-        seconds = Math.floor(timeRemaining % 60),
-        minutes = Math.floor((timeRemaining / 60) % 60),
-         hours = Math.floor((timeRemaining / 60 / 60));
-        return {
-            timeRemaining, hours,minutes,seconds
-        };
-        };
 
-       
-           
-            
-      function updateClock() { 
-            let timer = getTimeRemaining();
-            
+        function getTimeRemaining() {
+            const dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor((timeRemaining / 60 / 60));
+            return {
+                timeRemaining, hours, minutes, seconds
+            };
+        }
+
+        function updateClock() {
+            const timer = getTimeRemaining();
+
             function checkTime(i) {
-                if (i<10) {
-                i="0" + i;
-            } 
-            return i;
-        };
-            
-        
+                if (i < 10) {
+                    i = "0" + i;
+                }
+                return i;
+            }
 
             timerHours.textContent = checkTime(timer.hours);
             timerMinutes.textContent = checkTime(timer.minutes);
             timerSeconds.textContent = checkTime(timer.seconds);
-           
-        if(timer.timeRemaining <= 0) {
-            timerHours.textContent = '00';
-            timerMinutes.textContent = '00';
-            timerSeconds.textContent = '00';
-           clearInterval(end);
-        } 
-            
-        }    
-        
-      
-       let end = setInterval(updateClock, 1000);
-        
 
+            if (timer.timeRemaining <= 0) {
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+                clearInterval(end);
+            }
+        }
+
+        const end = setInterval(updateClock, 1000);
     }
 
-   countTimer('20 july 20');   
-   
-   
+    countTimer('20 july 20');
+
+
     // меню
 
     const toggleMenu = () => {
-        const btnMenu = document.querySelector('.menu');
-        let menu = document.querySelector('menu'),
-             body =  document.querySelector('body'),
-            closeBtn = document.querySelector('.close-btn'),
-            menuItems = menu.querySelectorAll('ul>li');
-                     
-            const handlerMenu = () => {               
-                menu.classList.toggle('active-menu');                 
+        const menu = document.querySelector('menu'),
+            body =  document.querySelector('body');
+
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+
+        body.addEventListener('click', (event) => {
+            const target = event.target;
+            if (target.closest('.menu')) {
+                handlerMenu();
+            } else if (target.closest('.active-menu') && target.tagName !== 'MENU') {
+                handlerMenu();
+            } else if (target.tagName !== 'MENU') {
+                menu.classList.remove('active-menu');
             }
-
-
-            body.addEventListener('click', (event) => {                
-                let target = event.target;  
-                if(target.closest('.menu')) {
-                    handlerMenu(); 
-                } else if (target.closest('.active-menu') && target.tagName !== 'MENU' ) {
-                    handlerMenu(); 
-                } else if (target.tagName !== 'MENU'){
-                    menu.classList.remove('active-menu')
-                };        
-            });
-
-
-            
-
-   
-  
+        });
     };
 
     toggleMenu();
 
 
 
-// popup
-let popupContent = document.querySelector('.popup-content');
+    // popup
+    const popupContent = document.querySelector('.popup-content');
     const togglePopUp = () => {
         const popup = document.querySelector('.popup');
         const popupBtn = document.querySelectorAll('.popup-btn');
-           
-            
-        
+
         popupBtn.forEach((elem) => {
             elem.addEventListener('click', () => {
+
                 popup.style.display = 'block';
                 animationPopup();
-               
-                
+
             });
         });
 
         popup.addEventListener('click', (event) => {
             let target = event.target;
-            if(target.classList.contains('popup-close')) {
+            if (target.classList.contains('popup-close')) {
                 popup.style.display = 'none';
             } else {
                 target = target.closest('.popup-content');
                 if (!target) {
                     popup.style.display = 'none';
                 }
-            };
-                
+            }
         });
     };
 
     togglePopUp();
 
     let count = 1;
-    let animationPopup = () => {
-        let clientWidth = document.documentElement.clientWidth;    
-        let goAnimation; 
+    const animationPopup = () => {
+        let clientWidth = document.documentElement.clientWidth;
+        let goAnimation;
 
         count += 1;
-        
-        popupContent.style.left = count + '%'; 
 
-        if (count < 38 && clientWidth > 768){
-            goAnimation = setTimeout(animationPopup, 15); 
-        } else if (clientWidth < 768){
+        popupContent.style.left = count + '%';
+
+        if (count < 38 && clientWidth > 768) {
+            goAnimation = setTimeout(animationPopup, 15);
+        } else if (clientWidth < 768) {
             popupContent.style.left = 23 + '%';
-            } else {
+        } else {
             clearInterval(goAnimation);
             count = 1;
-        }  
+        }
     };
 
 
-    // табы 
+    // табы
     const tabs = () => {
         const tabHeader = document.querySelector('.service-header'),
             tab = tabHeader.querySelectorAll('.service-header-tab'),
             tabContent = document.querySelectorAll('.service-tab');
-        
+
         const toggleTabContent = (index) => {
-            
-           for (let i=0; i < tabContent.length; i++) {
+
+            for (let i = 0; i < tabContent.length; i++) {
                 if (index === i) {
                     tab[i].classList.add('active');
                     tabContent[i].classList.remove('d-none');
@@ -164,30 +141,130 @@ let popupContent = document.querySelector('.popup-content');
                     tabContent[i].classList.add('d-none');
                     tab[i].classList.remove('active');
                 }
-           } 
+            }
         };
-        
+
         tabHeader.addEventListener('click', (event) => {
             let target = event.target;
-                target = target.closest('.service-header-tab');
+            target = target.closest('.service-header-tab');
 
-                if(target) {
+            if (target) {
 
-                    tab.forEach((item, i) => {                
-                         if (item === target) {
-                             toggleTabContent(i);
-                        }
+                tab.forEach((item, i) => {
+                    if (item === target) {
+                        toggleTabContent(i);
+                    }
                 });
-               
+
             }
-            
-        
         });
 
     };
     tabs();
 
+    //слайдер
 
+
+    function getDots() {
+        const portfolioContent = document.querySelector('.portfolio-content');
+
+        let dots = document.createElement('ul');
+        dots.classList.add('portfolio-dots');
+        portfolioContent.appendChild(dots);
+
+        const slide = document.querySelectorAll('.portfolio-item');
+        for (let i = 0; i < slide.length; i++) {
+            let dot = document.createElement('li');
+            dot.classList.add('dot');
+            dots.appendChild(dot);
+        }
+    };
+
+    getDots();
+
+    const slider = () => {
+        const slide = document.querySelectorAll('.portfolio-item'),
+            btn = document.querySelectorAll('.portfolio-btn'),
+            dot = document.querySelectorAll('.dot'),
+            slider = document.querySelector('.portfolio-content');
+
+        let currentSlide = 0;
+        let interval;
+
+        const prevSlide = (elem, index, strClass) => {
+            elem[index].classList.remove(strClass);
+        };
+
+        const nextSlide = (elem, index, strClass) => {
+            elem[index].classList.add(strClass);
+        };
+        const autoPlaySlide = () => {
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+            currentSlide++;
+            if (currentSlide >= slide.length) {
+                currentSlide = 0;
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+        };
+
+        const startSlide = (time = 3000) => {
+            interval = setInterval(autoPlaySlide, time);
+        };
+
+        const stopSlide = () => {
+            clearInterval(interval);
+        };
+
+        slider.addEventListener('click', (event) => {
+            event.preventDefault();
+            let target = event.target;
+
+            if (!target.matches('.portfolio-btn, .dot')) {
+                return;
+            }
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+            if (target.matches('#arrow-right')) {
+                currentSlide++;
+            } else if (target.matches('#arrow-left')) {
+                currentSlide--;
+
+            } else if (target.matches('.dot')) {
+                dot.forEach((elem, index) => {
+                    if (elem === target) {
+                        currentSlide = index;
+                    }
+                });
+            }
+            if (currentSlide >= slide.length) {
+                currentSlide = 0;
+            }
+
+            if (currentSlide < 0) {
+                currentSlide = slide.length - 1;
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+
+        });
+        startSlide(1500);
+
+        slider.addEventListener('mouseover', (event) => {
+            if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+                stopSlide();
+            }
+        });
+        slider.addEventListener('mouseout', (event) => {
+            if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+                startSlide();
+            }
+        });
+
+    };
+    slider();
 
 });
+
 
